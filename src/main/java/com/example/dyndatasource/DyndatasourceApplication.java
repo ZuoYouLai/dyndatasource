5911d89b72929a3e8d71987850c40af281916f5c
samlai <472023527@qq.com>
2019-07-24 00:34:34 +0800

    ll

--- src/main/java/com/example/dyndatasource/DyndatasourceApplication.java
+++ src/main/java/com/example/dyndatasource/DyndatasourceApplication.java
@@ -1,20 +1,12 @@
 package com.example.dyndatasource;
 
 import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
-import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication(exclude = DruidDataSourceAutoConfigure.class)
-@EnableScheduling
-@EnableTransactionManagement
-@Slf4j
 @MapperScan("com.example.dyndatasource.mapper")
-@EnableCaching
 public class DyndatasourceApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/dyndatasource/service/impl/TestServiceImpl.java
+++ src/main/java/com/example/dyndatasource/service/impl/TestServiceImpl.java
@@ -4,22 +4,23 @@ import com.baomidou.dynamic.datasource.annotation.DS;
 import com.example.dyndatasource.domain.SubCustomersExample;
 import com.example.dyndatasource.mapper.SubCustomersMapper;
 import com.example.dyndatasource.service.TestService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
 import java.util.List;
 
 
 
-@DS("temp")
+@DS("slave_1")
 @Service
 public class TestServiceImpl implements TestService {
 
 
-    @Autowired
+    @Resource
     private SubCustomersMapper subCustomersMapper;
 
 
+    @DS("slave_1")
     @Override
     public List list() {
         return subCustomersMapper.selectByExample(new SubCustomersExample());
